f6c34494cf8eac216ef1575e889d02ea530b1dba
filipeleasing <filipepaulocs@gmail.com>
2022-10-21 07:35:18 -0300

    Adicionar arquivos de projeto.

---
+++ JogoForca/adiciona_palavra.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "le_arquivo.hpp"
+#include "salva_arquivo.hpp"
+
+void adiciona_palavra(){
+    std::cout << "Digite a nova palavra, usando letras maiúsculas." << std::endl;
+    std::string nova_palavra,entrada;
+    std::cin >> nova_palavra;
+
+    /*nova_palavra = toupper(entrada);*/
+
+    std::vector<std::string> lista_palavras = le_arquivo();
+    lista_palavras.push_back(nova_palavra);
+
+    salva_arquivo(lista_palavras);
+}
